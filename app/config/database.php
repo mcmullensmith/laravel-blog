b0309a28245b7eb6d4eea5214c631402b6a3ffd3
Matthew Smith <mcmullensmith@gmail.com>
2015-02-02 11:26:37 -0500

    changed the env constant for user to DB_USER

--- app/config/database.php
+++ app/config/database.php
@@ -50,7 +50,7 @@ return array(
 			'driver'    => 'mysql',
             'host'      => $_ENV['DB_HOST'],
             'database'  => $_ENV['DB_NAME'],
-            'username'  => $_ENV['DB_USERNAME'],
+            'username'  => $_ENV['DB_USER'],
             'password'  => $_ENV['DB_PASSWORD'],
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
